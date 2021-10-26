82797cb5fc68e887727a9eb1c9e2ee6efbe6d4e6
rohit <rohittripthi47k@.com>
2021-10-26 23:33:29 +0530

    hello

---
+++ Rohit.java
@@ -0,0 +1,14 @@
+class Rohit{
+int height;
+float weight;
+public static void main(String[]args){
+Rohit r=new Rohit();
+r.fitness();
+}
+public void fitness(){
+height=5;
+weight=60;
+System.out.println("My height is =" + height+" "+"feet");
+System.out.println("My weight is =" + weight+" "+"kg");
+}
+}
